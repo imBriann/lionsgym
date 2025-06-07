5a7fa1430ca653ce8b6b7626a6b5f6e812ba7f51
Geron03 <149036182+Geron03@users.noreply.github.com>
2025-06-07 09:58:41 -0500

    Delete Backend directory
    Backend removed to improve errors

--- Backend/src/main/java/com/lionsgym/webApp/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lionsgym.webApp.repository;
-
-import com.lionsgym.webApp.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    boolean existsByEmail(String email); // Para validación si lo necesitas
-}
